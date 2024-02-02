bba30047889c60d3d6f6dc9dd68da3b782c3144e
nunu1101 <soobinnunu1101@gmail.com>
2024-02-02 21:57:55 +0900

    commit0202

--- src/main/java/com/ohgiraffers/practice/basic/Application3.java
+++ src/main/java/com/ohgiraffers/practice/basic/Application3.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.practice.basic;
 
+import java.util.Enumeration;
+
 public class Application3 {
     public static void main(String[] args) {
 
@@ -12,7 +14,16 @@ public class Application3 {
          * 1부터 10까지 짝수의 합 : 30
          * */
 
-        A3 a = new A3();
-        a.practiceApplication3();
+//        A3 a = new A3();
+//        a.practiceApplication3();
+        int[] arr = new int[10];
+
+        for(int i = 10; i <= arr.length; i--){
+            System.out.print(i + " ");
+            if (i ==1){
+                break;
+            }
+//            System.out.print(" ");
+        }
     }
 }
